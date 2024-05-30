0df25afd2fe63f027b5c073208cd9a67669d0a0c
Bikash Malu <141820540+Bikash-Malu@users.noreply.github.com>
2024-05-30 18:30:50 +0530

    chnages

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose"
 const connnectMongoDb=()=>{
     mongoose.connect(process.env.MONGO_DB_URL,{
-        
+        useNewUrlParser:true,
+        useUnifiedTopology:true
     }).then(()=>{
         console.log("connected")
     }).catch((error)=>{
