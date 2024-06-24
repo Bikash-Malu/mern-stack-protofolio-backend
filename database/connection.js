ce7fc1bf8fcd0bf5d018174746dd88b7f7c1efcb
Bikash Malu <141820540+Bikash-Malu@users.noreply.github.com>
2024-06-24 23:48:32 +0530

    adding

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 const connnectMongoDb=async()=>{
     try {
-        await mongoose.connect("mongodb+srv://bikashpage:8To7YI610MX0oYlA@cluster0.2jn7key.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+        await mongoose.connect("mongodb+srv://210301120093:jxQFnrQe4dgDpBZG@cluster0.rfoa62y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
           useNewUrlParser: true,
           useUnifiedTopology: true,
         })
